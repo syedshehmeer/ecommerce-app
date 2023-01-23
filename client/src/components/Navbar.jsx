097ef29f50f81ba6381d7d0ce88a38be5b85fc96
Syed Shehmeer Ashfaq <syedshehmeerashfaq@gmail.com>
2023-01-23 05:54:46 +0500

    Navigation Bar Added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import "../CSS/Navbar.css";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="navbar__left">
+        <img src="https://z.nooncdn.com/s/app/com/noon/design-system/logos/noon-logo-en.svg" />
+        <div className="navbar__country">
+          <img src="https://z.nooncdn.com/s/app/com/common/images/flags/sa.svg" />
+          <p>Deliver to Riyadh</p>
+        </div>
+      </div>
+      <div className="navbar__center">
+        <input placeholder="What are you looking for?" />
+      </div>
+      <div className="navbar__right">
+        <div className="navbar__menu">Arabic</div>
+        <div className="navbar__menu">
+          <strong>Sign In</strong>
+          <PersonOutlineIcon />
+        </div>
+        <div className="navbar__menu">
+          <strong>Cart</strong>
+          <ShoppingCartOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
