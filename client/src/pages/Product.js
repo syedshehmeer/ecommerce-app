f80976d755e9e0e2f369639a7273c3b1cc93ca9b
Syed Shehmeer Ashfaq <syedshehmeerashfaq@gmail.com>
2023-04-02 21:59:05 +0500

    Add loading on home

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Grid from "@mui/material/Grid";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 import "../CSS/Product.css";
 import ProductCard from "./ProductCard";
 
 const Product = () => {
   const [product, setProduct] = useState([]);
+  const [loading, isLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,7 +18,9 @@ const Product = () => {
         const response = await axios.get(
           "https://noonapp.onrender.com/api/products"
         );
+
         setProduct(response.data.products);
+        isLoading(false);
       } catch (error) {
         console.error(error);
       }
@@ -25,13 +30,22 @@ const Product = () => {
 
   return (
     <div>
-      <Grid container spacing={2} justifyContent="center" alignItems="center">
-        {product.map((prod) => (
-          <Grid item xs={8} sm={4} md={2} key={prod._id}>
-            <ProductCard prod={prod} />
-          </Grid>
-        ))}
-      </Grid>
+      {loading ? (
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      ) : (
+        <Grid container spacing={2} justifyContent="center" alignItems="center">
+          {product.map((prod) => (
+            <Grid item xs={8} sm={4} md={2} key={prod._id}>
+              <ProductCard prod={prod} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </div>
   );
 };
